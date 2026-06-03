648d128299f007d99ad8c8efe389d965d7cc3193
Huy-NTG <giahuy120304@gmail.com>
2026-06-03 09:54:26 +0700

    setup docker for frontend, backend, postgres

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres', // nếu chạy docker thì đổi thành 'postgres'
+      port: 5432,
+      username: 'postgres',
+      password: '123456',
+      database: 'VNADatabase',
+      autoLoadEntities: true,
+      synchronize: true, // dành cho dev
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
